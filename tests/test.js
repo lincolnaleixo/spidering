8614eead1768cd4d68c373d364d4ea90a4b7fe8d
Lincoln <lincolnmorais@gmail.com>
2020-05-14 12:12:37 +0200

    added args tests

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 /* eslint-disable max-lines-per-function */
 // const puppeteer = require('puppeteer')
 // const ProxyLists = require('proxy-lists')
@@ -5,19 +6,52 @@
 const Spider = require('../src/spidering');
 
 (async () => {
-	const pageType = 'clean'
-	const spider = new Spider()
-	const url = 'https://www.amazon.com/'
-	let response = ''
-	await spider.createBrowser()
-	await spider.createPage(pageType)
-	await spider.navigateTo(url)
-	response = await spider.scrape({ script: 'document.querySelector("#twotabsearchtextbox").name' })
-	console.log(`Using pupetter:${response}`)
-	await spider.closeBrowser()
+	let response
+	let pageType
+	let url
+	let script // 'document.querySelector("#twotabsearchtextbox").name'
+	let element // '#twotabsearchtextbox'
+	if (process.argv.find((item) => item === '--nodeEnv')) {
+		process.env.NODE_ENV = process.argv[process.argv.findIndex((item) => item === '--nodeEnv') + 1]
+	} else {
+		process.env.NODE_ENV = 'PRODUCTION'
+		console.log('Using default NODE_ENV PRODUCTION')
+	}
+	if (process.argv.find((item) => item === '--pageType')) {
+		pageType = process.argv[process.argv.findIndex((item) => item === '--pageType') + 1]
+	} else {
+		pageType = 'full'
+		console.log('Using default pageType full')
+	}
+	if (process.argv.find((item) => item === '--url')) {
+		url = process.argv[process.argv.findIndex((item) => item === '--url') + 1]
+	} else {
+		url = 'https://www.amazon.com'
+		console.log('Using default url Amazon.com')
+	}
 
-	response = await spider.scrape({
-		element: '#twotabsearchtextbox', url,
-	})
-	console.log(`Using axios:${response.attr('name')}`)
+	const spider = new Spider()
+
+	if (process.argv.find((item) => item === '--script')) {
+		script = process.argv[process.argv.findIndex((item) => item === '--script') + 1]
+		await spider.createBrowser()
+		await spider.createPage(pageType)
+		await spider.navigateTo(url)
+		response = await spider.scrape({ script })
+		console.log(`Response using pupetter: ${response}`)
+		await spider.closeBrowser()
+
+		return
+	}
+	console.log('No script set')
+
+	if (process.argv.find((item) => item === '--element')) {
+		element = process.argv[process.argv.findIndex((item) => item === '--element') + 1]
+		response = await spider.scrape({
+			element, url,
+		})
+		console.log(`Response using axios: ${response.attr('name')}`)
+	} else {
+		console.log('No element set')
+	}
 })()
